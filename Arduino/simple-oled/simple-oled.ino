527f3da0aa4887adc2e7ab5587641dce42728da9
sanjay0302 <sbsa597@gmail.com>
2024-11-24 14:32:22 +0530

    local

---
+++ Arduino/simple-oled/simple-oled.ino
@@ -0,0 +1,18 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_ADDR   0x3C
+Adafruit_SSD1306 display(128, 64, &Wire, -1);
+
+void setup() {
+  display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR);
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
+  display.setCursor(0, 0);
+  display.println("IP Address : ");
+  display.display();
+}
+
+void loop() {}
